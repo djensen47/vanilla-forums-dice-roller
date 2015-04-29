8b27f53ddaa5d30c6e863565395168fb996e98c0
David Jensen <david@jensen47.com>
2015-04-28 21:36:49 -0700

    Checkpoint from 4 years ago.

--- DiceRoller/class.diceroller.plugin.php
+++ DiceRoller/class.diceroller.plugin.php
@@ -11,6 +11,50 @@ $PluginInfo['DiceRoller'] = array(
    'AuthorUrl' => 'http://www.build47.com'
 );
 
+
+function rolldice($argstr) {
+    $args = explode(' ', $argstr);
+    $temp = '';
+    foreach($args as $arg) {
+        if (preg_match('/\d+d\d+/', $arg)) {
+            $dicesides = explode('d', $arg);
+            $dice = $dicesides[0];
+            $sides = $dicesides[1];
+        } else if (preg_match('/\d+/', $arg)) {
+            $dice = $arg;
+            $sides = 6;
+        } else {
+            $dice = -1;
+            $sides = -1;
+        }
+
+        if ($dice > -1 && $sides > -1) {
+            $dice_results = array();
+            for ($i=0; $i < $dice; $i++)
+            {
+                $dice_results[$i] = rand(1, $sides);
+            }
+            sort($dice_results);
+
+            $first = true;
+            $temp .= '(';
+            foreach($dice_results as $dv) {
+                if (!$first) {
+                    $temp .= ', ';
+                } else {
+                    $first = false;
+                }
+                $temp .= '' . $dv . '';
+            }
+            $temp .= ')&nbsp;&nbsp;';
+        } else {
+            $temp .= '(?)&nbsp;&nbsp;';
+        }
+    }
+
+    return '<div style="border: 1px solid #FF3333; background-color: white; padding: 0.5em;"><img src="/forums/Themes/axisandallies/dice-small.gif" border="0" alt="Dice" style="float: left;"/><b>Rolling ' . $argstr . ':</b><br />' . $temp . '</div>';
+    }
+
 class DiceRollerPlugin extends Gdn_Plugin {
 
     public function __construct() {
@@ -19,20 +63,23 @@ class DiceRollerPlugin extends Gdn_Plugin {
     public function CommentModel_BeforeSaveComment_Handler(&$Sender) {
         error_log($Sender->EventArguments['FormPostValues']);
     	$FormPostValues = $Sender->EventArguments['FormPostValues'];
-        $FormPostValues['Body'] = $FormPostValues['Body'] . " Hello world!";
+        $FormPostValues['Body'] = preg_replace('~:dice ([ 0-9d]+):~eis', "rolldice('\$1')", $FormPostValues['Body']);
         $Sender->EventArguments['FormPostValues'] = $FormPostValues;
     }
 
     public function DiscussionModel_BeforeSaveDiscussion_Handler(&$Sender) {
         error_log($Sender->EventArguments['FormPostValues']);
     	$FormPostValues = $Sender->EventArguments['FormPostValues'];
-        $FormPostValues['Body'] = $FormPostValues['Body'] . " Hello world!";
+        $FormPostValues['Body'] = preg_replace('~:dice ([ 0-9d]+):~eis', "$this->rolldice('\$1')", $FormPostValues['Body']);
+        
         $Sender->EventArguments['FormPostValues'] = $FormPostValues;
     }
 
     public function Setup(){
         error_log('DiceRollerPlugin Setup');
     }
+
+
 }
 
 ?>
